b2eee99016c704266827a88801f056a522dd223b
ams <amirhoseinmehrabi765@gmail.com>
2023-04-19 17:05:23 +0430

    add new styles and icons to rightbar

--- comps/rightBar.js
+++ comps/rightBar.js
@@ -1,7 +1,51 @@
-const Rightbar = ()=>{
-    return(
+import { Box, Checkbox, FormControlLabel, FormGroup, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import { Inbox, Drafts, Circle } from '@mui/icons-material';
+const Rightbar = () => {
+    return (
         <>
-        <h1>Rightbar</h1>
+            <h1>todo list</h1>
+            <Box sx={{ width: '100%', maxWidth: 360, bgcolor: '' }}>
+                <nav aria-label="main mailbox folders">
+                    <List sx={{ padding: "1rem" }}>
+                        <ListItem >
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Circle sx={{color:"skyblue"}}/>
+                                </ListItemIcon>
+                                <ListItemText primary="work" />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem >
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Circle sx={{color:"blue"}}/>
+                                </ListItemIcon>
+                                <ListItemText primary="study" />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem >
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Circle sx={{color:"wheat"}}/>
+                                </ListItemIcon>
+                                <ListItemText primary="family" />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem >
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Circle sx={{color:"greenyellow"}}/>
+                                </ListItemIcon>
+                                <ListItemText primary="erntertaiment" />
+                            </ListItemButton>
+                        </ListItem>
+                        <FormGroup >
+                            <FormControlLabel sx={{ ml: 3,mt:1 }} control={<Checkbox />} label={<Typography sx={{ fontSize: ".8rem" }}>hide done tasks</Typography>} />
+                        </FormGroup>
+                    </List>
+                </nav>
+            </Box>
+
         </>
     )
 }
