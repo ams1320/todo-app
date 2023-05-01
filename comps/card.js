4028b919760e5d4d8518b23809f5c31d04b3434e
ams <amirhoseinmehrabi765@gmail.com>
2023-05-01 19:25:30 +0430

    bullshit

--- comps/card.js
+++ comps/card.js
@@ -1,88 +1,71 @@
 import { Button, Card, CardActions, CardContent, Grid, Typography, Box, Stack, FormGroup, FormControlLabel, Checkbox, ListItemIcon } from '@mui/material';
 import { MoreHoriz, Circle, Edit, Delete } from '@mui/icons-material';
 import Style from "../styles/card.module.css"
-import { useEffect, useState } from 'react';
 
 const Cards = () => {
 
     const list = ["work", "study", "family", "entertainment"];
 
-    const [State, Setstate] = useState({
-        task:[],
-    })
-
-    useEffect(() => {
-        
-            const tasks = JSON.parse(localStorage.getItem("task"))
-            Setstate({task:tasks})
-       
-    }, [])
-
-    const handledelete =(indexTask)=>{
-        let todos= JSON.parse(localStorage.getItem("task"));
-            todos.filter((todo,index)=>{
-                index !== indexTask;
-            })
-            let task= !localStorage.setItem("task",JSON.stringify(todos))?[]:localStorage.setItem("task",JSON.stringify(todos))
-            Setstate({Tasks:task})
-        }
-
     return (
         <>
             <Box>
                 <Grid container >
-                    {!State.task ? <Typography variant='h4'>nothing here</Typography> :
-                        State.task.map((task,index) => {
-                            
-                            return (
-                                <Grid sm={6} mb={2}>
-                                    <Card sx={{ maxWidth: 460 }} className={Style.card}>
-                                        <CardContent>
-                                            <Stack direction='row' justifyContent="space-between">
-                                                <Typography variant='h5' sx={{ fontWeight: "700", textDecorationLine: task.done ? "line-through" : "none" }} className='title'>{task.title}</Typography>
-                                                <Button data-bs-toggle="dropdown" aria-expanded="false"><MoreHoriz /></Button>
-                                                <ul className="dropdown-menu" >
-                                                    <Button className="dropdown-item" ><Edit fontSize='1rem' /> edit</Button>
-                                                    <Button className="dropdown-item"  onClick={handledelete(index)}><Delete fontSize='1rem' /> delete</Button>
-                                                </ul>
-                                            </Stack>
-                                            <Typography variant='p' sx={{ paddingTop: 10, fontSize: "1.1rem", fontWeight: "600" }}>{task.info}</Typography>
-                                        </CardContent>
-                                        <CardActions sx={{ display: "grid" }}>
-                                            <Stack direction="row" justifyContent="space-between">
-                                                <Box>
+                    <Grid sm={6}>
+                        <Card sx={{ maxWidth: 490 }} className={Style.card}>
+                            <CardContent>
+                                <Stack direction='row' justifyContent="space-between">
+                                    <Typography variant='h5' sx={{ fontWeight: "700" }}>card title</Typography>
+                                    <Button data-bs-toggle="dropdown" aria-expanded="false"><MoreHoriz /></Button>
+                                    <ul className="dropdown-menu" >
+                                        <li className="dropdown-item"><Edit fontSize='1rem'/> edit</li>
+                                        <li className="dropdown-item"><Delete fontSize='1rem'/> delete</li>
+                                    </ul>
+                                </Stack>
+                                <Typography variant='p' sx={{ paddingTop: 10, fontSize: "1.1rem", fontWeight: "600" }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto eum nisi similique provident excepturi labore a est, cum neque officia!</Typography>
+                            </CardContent>
+                            <CardActions sx={{ direction: "rtl" }}>
+                                <Stack direction='row-reverse' gap={26} flexItem>
+                                    <Stack direction='row'>
 
-                                                    {
-                                                        task.type == "work" ? <Circle sx={{ color: "skyblue", fontSize: "2.5rem" }} titleAccess='work' /> :
-                                                        task.type == "study" ? <Circle sx={{ color: "blue", fontSize: "2.5rem" }} titleAccess='study' /> :
-                                                        task.type == "family" ? <Circle sx={{ color: "wheat", fontSize: "2.5rem" }} titleAccess='family' /> :
-                                                                    <Circle sx={{ color: "greenyellow", fontSize: "2.5rem" }} titleAccess='entertainment' />
-                                                  }
-                                                </Box>
+                                        {list.map((tit) => (
+                                            tit == "work" ? <Circle sx={{ color: "skyblue", fontSize: "2.5rem" }} titleAccess='work' /> :
+                                                tit == "study" ? <Circle sx={{ color: "blue", fontSize: "2.5rem" }} titleAccess='study' /> :
+                                                    tit == "family" ? <Circle sx={{ color: "wheat", fontSize: "2.5rem" }} titleAccess='family' /> :
+                                                        <Circle sx={{ color: "greenyellow", fontSize: "2.5rem" }} titleAccess='entertainment' />
+                                        ))}
 
-                                                <FormGroup >
-                                                    <FormControlLabel control={<Checkbox onClick={() => {
+                                    </Stack>
+                                    <FormGroup >
+                                        <FormControlLabel sx={{ direction: "ltr" }} control={<Checkbox />} label="Done" />
+                                    </FormGroup>
+                                </Stack>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid sm={6}>
+                        <Card sx={{ maxWidth: 490 }} className={Style.card}>
+                            <CardContent>
+                                <Stack direction='row' justifyContent="space-between">
+                                    <Typography variant='h5' sx={{ fontWeight: "700" }}>card title</Typography>
+                                    <Button ><MoreHoriz /></Button>
+                                </Stack>
+                                <Typography variant='p' sx={{ paddingTop: 10, fontSize: "1.1rem", fontWeight: "600" }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic dicta veniam ipsam fugiat repellendus id iste? Nisi impedit dolore maiores amet beatae praesentium possimus, ipsa soluta temporibus laboriosam, provident reprehenderit porro cupiditate corrupti expedita nesciunt obcaecati, numquam distinctio quae nihil.</Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Stack justifyContent="flex-end">
+                                    <FormGroup >
+                                        <FormControlLabel control={<Checkbox />} label="Done" />
+                                    </FormGroup>
 
-                                                        // let tit = document.querySelector(".title")
-                                                        // tit.style.textDecorationLine == "line-through" ? tit.style.textDecorationLine = "none" : tit.style.textDecorationLine = "line-through"
-                                        
-                                                   }} />} label="Done" />
-                                                </FormGroup>
-                                            </Stack>
-                                        </CardActions>
-                                    </Card>
-                                </Grid>
-                            )
-                        })
-                    }
-
+                                </Stack>
+                            </CardActions>
+                        </Card>
+                    </Grid>
 
                 </Grid>
             </Box>
         </>
     )
-
-   
 
 }
 
