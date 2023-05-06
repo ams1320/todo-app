3baa16585ed3cda538f125061cccdc31fe3c86f0
ams <amirhoseinmehrabi765@gmail.com>
2023-05-06 13:16:23 +0430

    add reducer localstorag

--- redux/taskReducer.js
+++ redux/taskReducer.js
@@ -1,18 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const TaskSlice =createSlice({
-    name:"store",
-    initialState:{
-        task :[]
-    },
-    reducers:{
-        updateBasket:(state,action)=>{
 
-            state.task = [...state.task,action.payload]
+
+const TaskSlice = createSlice({
+    name: "store",
+    
+    initialState: {
+        task:typeof window !== "undefined" ? JSON.parse(window.localStorage.getItem('task')) : [],
+    },
+    reducers: {
+        updateBasket: (state, action) => {
+
+            let todos = !window.localStorage.getItem('task') ? [] : JSON.parse(window.localStorage.getItem('task'));
+            todos.push(action.payload)
+            state.task = todos;
+
             console.log(state.task)
+            // let todos = [...state.task,action.payload]
+
         }
     }
 })
 
-export const {updateBasket} = TaskSlice.actions;
+export const { updateBasket } = TaskSlice.actions;
 export default TaskSlice.reducer;
