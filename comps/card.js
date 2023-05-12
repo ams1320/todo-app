be4f931a53927c6104793c28a2b41aab2c5960e7
ams <amirhoseinmehrabi765@gmail.com>
2023-05-12 20:37:26 +0430

    add edit and done reducers

--- comps/card.js
+++ comps/card.js
@@ -1,46 +1,143 @@
-import { Button, Card, CardActions, CardContent, Grid, Typography, Box, Stack, FormGroup, FormControlLabel, Checkbox, ListItemIcon } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Grid, Typography, Box, Stack, FormGroup, FormControlLabel, Checkbox, ListItemIcon, TextField } from '@mui/material';
 import { MoreHoriz, Circle, Edit, Delete } from '@mui/icons-material';
 import Style from "../styles/card.module.css"
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteTask } from '@/redux/taskReducer';
+import { deleteTask, editTask , doneTask } from '@/redux/taskReducer';
+import { Form, Formik, Field } from 'formik';
+import * as yup from "yup";
 
 const Cards = () => {
 
 
     const dispatch = useDispatch();
-    const tasks = useSelector((state)=> state.store.task)
+    const tasks = useSelector((state) => state.store.task)
 
-    
 
-    const [State, Setstate] = useState({
-        Tasks: []
-    })
 
-    useEffect(() => {
-        const tasks = JSON.parse(localStorage.getItem("task"))
-        Setstate({ Tasks: tasks })
 
-    }, [])
     return (
         <>
             <Box>
                 <Grid container >
                     {!tasks ? <Typography variant='h4'>nothing here</Typography> :
-                        tasks.map((task,index) => {
+                        tasks.map((task, index) => {
                             return (
-                                <Grid sm={6} mb={1}>
+                                <Grid sm={6} mb={2}>
                                     <Card sx={{ maxWidth: 460 }} className={Style.card}>
                                         <CardContent>
                                             <Stack direction='row' justifyContent="space-between">
                                                 <Typography variant='h5' sx={{ fontWeight: "700", textDecorationLine: task.done ? "line-through" : "none" }} className='title'>{task.title}</Typography>
                                                 <Button data-bs-toggle="dropdown" aria-expanded="false"><MoreHoriz /></Button>
                                                 <ul className="dropdown-menu" >
-                                                    <li className="dropdown-item"><Edit fontSize='1rem' /> edit</li>
-                                                    <li className="dropdown-item"><Button onClick={()=>{
-                                                        dispatch(deleteTask(index))
-                                                    }}><Delete fontSize='1rem' /> delete</Button></li>
+                                                    <li className="dropdown-item"><Button data-bs-toggle="modal" data-bs-target={`#staticBackdrop${index}`}><Edit fontSize='1rem' /> edit</Button></li>
+
+                                                    <li className="dropdown-item"><Button onClick={() => dispatch(deleteTask(index))}><Delete fontSize='1rem' /> delete</Button></li>
                                                 </ul>
+                                                <div class="modal fade" id={`staticBackdrop${index}`} data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <Formik
+                                                                    validationSchema={yup.object().shape({
+                                                                        type: yup
+                                                                            .string('enter the type ')
+                                                                            .required('type of task is required'),
+                                                                        title: yup
+                                                                            .string('enter your task title')
+                                                                            .min(1, 'title at least should have 1 character length')
+                                                                            .required('title is required'),
+                                                                        info: yup
+                                                                            .string('Enter your task info')
+                                                                            .min(8, 'information should be of minimum 8 characters length')
+                                                                            .required('information is required'),
+
+                                                                    })}
+                                                                    initialValues={
+                                                                        {
+                                                                            type: task.type,
+                                                                            title: task.title,
+                                                                            info: task.info,
+                                                                            done: task.done,
+                                                                        }}
+                                                                    onSubmit={async (values) => {
+
+                                                                        dispatch(editTask({ index, task: values }))
+                                                                    }}
+                                                                    render={(formikBag) => (
+                                                                        <Form style={{}} onSubmit={formikBag.handleSubmit} className="form-container">
+                                                                            <Grid container mr={1}>
+                                                                                <Grid sm={12} xs={12} mb={2} mt={2}>
+                                                                                    <Field as="select" name="type" style={{ boxShadow: "2px 2px 3px grey", borderRadius: 2, width: 400,height:40 }} >
+                                                                                        
+                                                                                        <option value="work">work</option>
+                                                                                        <option value="study">study</option>
+                                                                                        <option value="family">family</option>
+                                                                                        <option value="entertaiment">entertaiment</option>
+                                                                                    </Field>
+
+                                                                                </Grid>
+                                                                                <Grid sm={12} mt={2}>
+                                                                                    <Field render={({ field, form }) => (
+                                                                                        <TextField
+                                                                                            label="task title"
+                                                                                            name="title"
+                                                                                            error={
+                                                                                                Boolean(form.errors.title && form.touched.title)
+                                                                                            }
+                                                                                            sx={{ boxShadow: "3px 3px 4px grey", width: 360, ml: 2, borderRadius: 2 }}
+                                                                                            InputLabelProps={{ style: { fontSize: 15, fontWeight: 500, color: "black" } }}
+                                                                                            onChange={formikBag.handleChange}
+                                                                                            onBlur={formikBag.handleBlur}
+                                                                                            helperText={
+                                                                                                form.errors.title &&
+                                                                                                form.touched.title &&
+                                                                                                String(form.errors.title)
+                                                                                            }
+                                                                                        />
+
+                                                                                    )} />
+                                                                                </Grid>
+                                                                                <Grid sm={12} mt={2}>
+                                                                                    <Field render={({ field, form }) => (
+                                                                                        <TextField
+                                                                                            label="task information"
+                                                                                            name="info"
+                                                                                            error={
+                                                                                                Boolean(form.errors.info && form.touched.info)
+                                                                                            }
+                                                                                            sx={{ boxShadow: "3px 3px 4px grey", width: 360, ml: 2, borderRadius: 2 }}
+                                                                                            InputLabelProps={{ style: { fontSize: 15, fontWeight: 500, color: "black" } }}
+                                                                                            onChange={formikBag.handleChange}
+                                                                                            onBlur={formikBag.handleBlur}
+                                                                                            helperText={
+                                                                                                form.errors.info &&
+                                                                                                form.touched.info &&
+                                                                                                String(form.errors.info)
+                                                                                            }
+                                                                                        />
+
+                                                                                    )} />
+                                                                                </Grid>
+                                                                                <Grid sm={12} xs={12} mt={7} >
+                                                                                    <div className="modal-footer">
+                                                                                        <Button color="success" variant="contained" fullWidth type="submit" sx={{ bottom: "0%", top: "12%" }}>
+                                                                                            Submit
+                                                                                        </Button>
+                                                                                    </div>
+                                                                                </Grid>
+                                                                            </Grid>
+                                                                        </Form>)}>
+                                                                </Formik>
+                                                            </div>
+                                                            
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </Stack>
                                             <Typography variant='p' sx={{ paddingTop: 10, fontSize: "1.1rem", fontWeight: "600" }}>{task.info}</Typography>
                                         </CardContent>
@@ -50,19 +147,14 @@ const Cards = () => {
 
                                                     {
                                                         task.type == "work" ? <Circle sx={{ color: "skyblue", fontSize: "2.5rem" }} titleAccess='work' /> :
-                                                        task.type == "study" ? <Circle sx={{ color: "blue", fontSize: "2.5rem" }} titleAccess='study' /> :
-                                                        task.type == "family" ? <Circle sx={{ color: "wheat", fontSize: "2.5rem" }} titleAccess='family' /> :
+                                                            task.type == "study" ? <Circle sx={{ color: "blue", fontSize: "2.5rem" }} titleAccess='study' /> :
+                                                                task.type == "family" ? <Circle sx={{ color: "wheat", fontSize: "2.5rem" }} titleAccess='family' /> :
                                                                     <Circle sx={{ color: "greenyellow", fontSize: "2.5rem" }} titleAccess='entertainment' />
-                                                  }
+                                                    }
                                                 </Box>
 
                                                 <FormGroup >
-                                                    <FormControlLabel control={<Checkbox onClick={() => {
-
-                                                        // let tit = document.querySelector(".title")
-                                                        // tit.style.textDecorationLine == "line-through" ? tit.style.textDecorationLine = "none" : tit.style.textDecorationLine = "line-through"
-                                        
-                                                   }} />} label="Done" />
+                                                    <FormControlLabel  control={<Checkbox checked={!task.done ? false:true} onChange={()=>dispatch(doneTask({index , tasks: !task.done ?true : false}))}  />} label="Done" />
                                                 </FormGroup>
                                             </Stack>
                                         </CardActions>
@@ -74,7 +166,7 @@ const Cards = () => {
 
 
                 </Grid>
-            </Box>
+            </Box >
         </>
     )
 
--- redux/taskReducer.js
+++ redux/taskReducer.js
@@ -4,34 +4,45 @@ const TaskSlice = createSlice({
     name: "store",
 
     initialState: {
-        task:typeof window !== "undefined" ? JSON.parse(window.localStorage.getItem('task')) : [],
+        task: typeof window !== "undefined" ? JSON.parse(window.localStorage.getItem('task')) : [],
     },
     reducers: {
         updateBasket: (state, action) => {
 
             let todos = !window.localStorage.getItem('task') ? [] : JSON.parse(window.localStorage.getItem('task'));
             todos.push(action.payload)
-            localStorage.setItem("task",JSON.stringify(todos))
+            localStorage.setItem("task", JSON.stringify(todos))
             state.task = JSON.parse(localStorage.getItem('task'));
 
-            console.log(state.task)
-            // let todos = [...state.task,action.payload]
-
         },
-        deleteTask:(state,action)=>{
-           const  taskIndex = action.payload;
-           const todos = state.task.filter((value,index)=> {
-            // console.log(value.title)
-            // console.log(taskIndex)
-          return  index !== taskIndex
-        });;
-           
-           console.log(todos)
-           localStorage.setItem("task",JSON.stringify(todos));
-           state.task = JSON.parse(localStorage.getItem("task"));
+        deleteTask: (state, action) => {
+            const taskIndex = action.payload;
+            const todos = state.task.filter((value, index) => {
+                // console.log(value.title)
+                // console.log(taskIndex)
+                return index !== taskIndex
+            });;
+
+            console.log(todos)
+            localStorage.setItem("task", JSON.stringify(todos));
+            state.task = JSON.parse(localStorage.getItem("task"));
+        },
+        editTask: (state, action) => {
+            let task = JSON.parse(localStorage.getItem("task"));
+            const newTask = action.payload.task;
+
+            task[action.payload.index] = newTask;
+            localStorage.setItem('task', JSON.stringify(task));
+            state.task = JSON.parse(localStorage.getItem("task"));
+        },
+        doneTask: (state, action) => {
+            let task = JSON.parse(localStorage.getItem("task"));
+            task[action.payload.index].done = action.payload.tasks;
+            localStorage.setItem('task', JSON.stringify(task));
+            state.task = JSON.parse(localStorage.getItem("task"));
         }
     }
 })
 
-export const { updateBasket,deleteTask } = TaskSlice.actions;
+export const { updateBasket, deleteTask, editTask ,doneTask } = TaskSlice.actions;
 export default TaskSlice.reducer;
