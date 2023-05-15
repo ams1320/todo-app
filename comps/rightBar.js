8d9902b73c7804a1ba2051b54999817ae6f3c650
ams <amirhoseinmehrabi765@gmail.com>
2023-05-15 20:26:38 +0430

    add links to rightbar

--- comps/rightBar.js
+++ comps/rightBar.js
@@ -1,49 +1,56 @@
 import { Box, Checkbox, FormControlLabel, FormGroup, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import { Inbox, Drafts, Circle } from '@mui/icons-material';
 import Link from "next/link";
+import style from"@/styles/right.module.css"
 const Rightbar = () => {
     return (
         <>
- 
-            <Box sx={{ width: '100%', maxWidth: 220, bgcolor: '',paddingLeft:"2rem" }}>
+
+            <Box sx={{ width: '100%', maxWidth: 220, bgcolor: '', paddingLeft: "2rem" }}>
                 <nav aria-label="main mailbox folders">
-                    <List sx={{ }}>
+                    <List>
                         <ListItem >
-                            <Link href="/task/work">
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <Circle sx={{color:"skyblue"}}/>
-                                </ListItemIcon>
-                                <ListItemText primary="work" />
-                            </ListItemButton>
+                            <Link href="/task/work" className={style.link}>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <Circle sx={{ color: "skyblue" }} />
+                                    </ListItemIcon>
+                                    <ListItemText className={style.linkText} primary="work" />
+                                </ListItemButton>
                             </Link>
                         </ListItem>
                         <ListItem >
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <Circle sx={{color:"blue"}}/>
-                                </ListItemIcon>
-                                <ListItemText primary="study" />
-                            </ListItemButton>
+                            <Link href="/task/study" className={style.link}>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <Circle sx={{ color: "blue" }} />
+                                    </ListItemIcon>
+                                    <ListItemText primary="study"  className={style.linkText}/>
+                                </ListItemButton>
+                            </Link>
                         </ListItem>
                         <ListItem >
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <Circle sx={{color:"wheat"}}/>
-                                </ListItemIcon>
-                                <ListItemText primary="family" />
-                            </ListItemButton>
+                            <Link href="/task/family" className={style.link}>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <Circle sx={{ color: "wheat" }} />
+                                    </ListItemIcon>
+                                    <ListItemText primary="family" className={style.linkText}/>
+                                </ListItemButton>
+                            </Link>
                         </ListItem>
                         <ListItem >
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <Circle sx={{color:"greenyellow"}}/>
-                                </ListItemIcon>
-                                <ListItemText primary="erntertaiment" />
-                            </ListItemButton>
+                            <Link href="/task/entertaiment" className={style.link}>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <Circle sx={{ color: "greenyellow" }} />
+                                    </ListItemIcon>
+                                    <ListItemText primary="erntertaiment" className={style.linkText}/>
+                                </ListItemButton>
+                            </Link>
                         </ListItem>
                         <FormGroup >
-                            <FormControlLabel sx={{ ml: 3,mt:1 }} control={<Checkbox />} label={<Typography sx={{ fontSize: ".8rem" }}>hide done tasks</Typography>} />
+                            <FormControlLabel sx={{ ml: 3, mt: 1 }} control={<Checkbox />} label={<Typography sx={{ fontSize: ".8rem" }}>hide done tasks</Typography>} />
                         </FormGroup>
                     </List>
                 </nav>
